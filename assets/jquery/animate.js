e32bf1baee3c83f0fc0a63f92394a555bde33fb9
Vison <kk801030@gmail.com>
2014-08-12 02:15:48 +0800

    edit side-nav

--- assets/jquery/animate.js
+++ assets/jquery/animate.js
@@ -5,5 +5,15 @@ $('.title-bar').delay(300).animate({ opacity: 1 }, 300);
 //
 $('.side-nav li').click(function(){
 	$('.side-nav li').css({"border-left" : "solid 0px #fff"});
+	$('.side-nav li').find('a').css({"color": "#bbb"});
+	
 	$(this).css({"border-left" : "solid 2px #039b96"});
+	$(this).find('a').css({"color": "#039b96"});
+});
+
+$('.side-nav li').hover(function(){
+	$(this).find('a').css({"color": "#039b96"});
+	},
+	function(){
+	$(this).find('a').css({"color": ""});
 });
